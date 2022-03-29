444c2abb00fbd36db8785b228d7132a458b63fa0
CHR-onicles <tpandivine48@gmail.com>
2022-03-29 11:56:17 +0000

    Resolves #4

---
+++ data.js
@@ -0,0 +1,75 @@
+// --------------------------------------------------
+// Data
+// --------------------------------------------------
+
+const CLASSES = [
+    {
+        day: 'Monday',
+        courses: [
+            { name: '_Formal Methods_ | ⏰5:30pm | 🏠N3', duration: 2 }
+        ]
+    },
+    {
+        day: 'Tuesday',
+        courses: [
+            { name: '_Accounting_ | ⏰5:30pm | 🏠JQB23', duration: 2 }
+        ]
+    },
+    {
+        day: 'Wednesday',
+        courses: [
+            { name: '_Compilers_ | ⏰9:30am | 🏠E10', duration: 2 },
+            { name: '_Theory & Survey_ | ⏰3:30pm | 🏠JQB09', duration: 2 },
+            { name: '_Soft. Modelling_ | ⏰5:30pm | 🏠LOT1', duration: 2 }
+        ]
+    },
+    {
+        day: 'Thursday',
+        courses: [
+            { name: '_Project_ | ⏰8:30am | 🏠Online', duration: 2 },
+            { name: '_Formal Methods_ | ⏰12:30pm | 🏠JQB19', duration: 1 },
+            { name: '_Accounting_ | ⏰6:30pm | 🏠E10', duration: 1 }
+        ]
+    },
+    {
+        day: 'Friday',
+        courses: [
+            { name: '_Soft. Modelling_ | ⏰9:30am | 🏠N3', duration: 1 },
+            { name: '_Theory & Survey_ | ⏰10:30am | 🏠N3', duration: 1 },
+            { name: '_Compilers_ | ⏰4:30pm | 🏠NNB2', duration: 1 }
+        ]
+    }
+]
+
+const HELP_COMMANDS = [
+    {
+        command: "*!ping*",
+        desc: "check if I'm available 🙋🏽‍♂️"
+    },
+    {
+        command: "*!help*",
+        desc: "get commands that can be used with me 💡"
+    },
+    {
+        command: "*!mute*",
+        desc: "get me to be quiet 😅"
+    },
+    {
+        command: "*!unmute*",
+        desc: "allow me to talk 🙂"
+    },
+    {
+        command: "*!everyone*",
+        desc: "ping everyone in the group 😮"
+    },
+    {
+        command: "*!classes*",
+        desc: "get all the classes you have this week 📚"
+    },
+    {
+        command: "*!class*",
+        desc: "get today's classes 📕"
+    }
+]
+
+module.exports = { CLASSES, HELP_COMMANDS }
---
+++ helpers.js
@@ -0,0 +1,29 @@
+// --------------------------------------------------
+// Helper functions
+// --------------------------------------------------
+const LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./node-localStorage');
+
+
+const pickRandomReply = (replies) => {
+    return replies[Math.floor(Math.random() * replies.length)];
+}
+
+const getIsMutedStatus = () => {
+    return JSON.parse(localStorage.getItem('IS_MUTED') || false);
+}
+
+const extractTime = (course) => {
+    const time_portion = course.split('|')[1].trim();
+    const raw_time = time_portion.slice(1, time_portion.length);
+    let new_raw_time = null;
+
+    if (raw_time.includes('p') && !raw_time.includes('12')) {
+        const hour_24_format = +raw_time.split(':')[0] + 12;
+        new_raw_time = String(hour_24_format) + ':' + raw_time.split(':')[1];
+    }
+
+    return new_raw_time || raw_time;
+}
+
+module.exports = { pickRandomReply, getIsMutedStatus, extractTime, localStorage }
