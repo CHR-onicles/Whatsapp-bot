cb33a3a64dd9744844596935f4a715d79f3101a7
CHR-onicles <tpandivine48@gmail.com>
2022-06-29 23:59:45 +0000

    feat: Attempting to implement log feature

--- commands/admin/status.js
+++ commands/admin/status.js
@@ -7,52 +7,81 @@ const { NOT_BOT_ADMIN_REPLIES } = require("../../utils/data");
 const execute = async (client, msg, args) => {
     if (await getMutedStatus() === true) return;
 
-    const { BOT_START_TIME } = args;
+    const { BOT_START_TIME, RUN_FIRST_TIME } = args;
     if (!BOT_START_TIME) throw new Error('Invalid Bot Start Time');
-    const contact = await msg.getContact();
-    const isBotAdmin = await isUserBotAdmin(contact);
-    const { CSCD416, CSCD418, CSCD422, CSCD424, CSCD400, CSCD426, CSCD428, CSCD432, CSCD434 } = await getNotificationStatus();
-    const isForwardingOn = await getForwardingStatus();
+    let isBotAdmin = null;
+    if (!RUN_FIRST_TIME) {
+        // to prevent getting an error when running for the first time
+        const contact = await msg.getContact();
+        isBotAdmin = await isUserBotAdmin(contact);
+    }
 
-    if (isBotAdmin) {
-        const allChats = await client.getChats();
-        const blocked_chats = await client.getBlockedContacts();
-        const { groupChats, private_chats } = allChats.reduce((chats, chat) => {
-            if (chat.isGroup) chats.groupChats += 1;
-            else chats.private_chats += 1;
-            return chats;
-        }, { groupChats: 0, private_chats: 0 });
-        const allAnnouncements = await getAllAnnouncements();
-        const allLinks = await getAllLinks();
+    if (isBotAdmin || RUN_FIRST_TIME) {
 
-        const currentTime = new Date();
-        const { days, hours, minutes, seconds } = msToDHMS(currentTime - BOT_START_TIME);
-        let reply = ['▄▀▄▀  𝔹𝕆𝕋 𝕊𝕋𝔸𝕋𝕌𝕊  ▀▄▀▄\n'];
+        const generateReplies = async () => {
+            const { CSCD416, CSCD418, CSCD422, CSCD424, CSCD400, CSCD426, CSCD428, CSCD432, CSCD434 } = await getNotificationStatus();
+            const isForwardingOn = await getForwardingStatus();
+            const allChats = await client.getChats();
+            const blocked_chats = await client.getBlockedContacts();
+            const { groupChats, private_chats } = allChats.reduce((chats, chat) => {
+                if (chat.isGroup) chats.groupChats += 1;
+                else chats.private_chats += 1;
+                return chats;
+            }, { groupChats: 0, private_chats: 0 });
+            const allAnnouncements = await getAllAnnouncements();
+            const allLinks = await getAllLinks();
 
-        reply.push(`[🔰] *Environment:* ${currentEnv}`);
-        reply.push(`[🔰] *Platform:* ${process.platform}`);
-        reply.push(`[🔰] *Response time:* ${Math.abs(new Date() - new Date(msg.timestamp * 1000))}ms`);
-        reply.push(`[🔰] *Uptime:*${days ? ' ' + days : ''}${days ? (days === 1 ? 'day' : 'days') : ''}${hours ? ' ' + hours : ''}${hours ? (hours === 1 ? 'hr' : 'hrs') : ''}${minutes ? ' ' + minutes : ' 0mins'}${minutes ? (minutes === 1 ? 'min' : 'mins') : ''} ${seconds ? seconds : 0}secs`);
-        reply.push(`[🔰] *Ram:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${Math.round(totalmem / 1024 / 1024)} MB`);
-        reply.push(`[🔰] *Total chats:* ${allChats.length}`);
-        reply.push(`[🔰] *Group chats:* ${groupChats}`);
-        reply.push(`[🔰] *Private chats:* ${private_chats}`);
-        reply.push(`[🔰] *Blocked chats:* ${blocked_chats.length}`);
-        reply.push(`[🔰] *Announcements stored:* ${allAnnouncements.length}`);
-        reply.push(`[🔰] *Links stored:* ${allLinks.length}`);
-        reply.push(`[🔰] *Forwarding stuff status:* ${isForwardingOn ? "✅" : "❌"}\n`); // temporary - will be removed soon
+            const currentTime = new Date();
+            const { days, hours, minutes, seconds } = msToDHMS(currentTime - BOT_START_TIME);
 
-        reply.push(`[🔰] *CSCD416 notification status:* ${CSCD416 ? "✅" : "❌"}`);
-        reply.push(`[🔰] *CSCD418 notification status:* ${CSCD418 ? "✅" : "❌"}`);
-        reply.push(`[🔰] *CSCD422 notification status:* ${CSCD422 ? "✅" : "❌"}`);
-        reply.push(`[🔰] *CSCD424 notification status:* ${CSCD424 ? "✅" : "❌"}`);
-        reply.push(`[🔰] *CSCD400 notification status:* ${CSCD400 ? "✅" : "❌"}`);
-        reply.push(`[🔰] *CSCD426 notification status:* ${CSCD426 ? "✅" : "❌"}`);
-        reply.push(`[🔰] *CSCD428 notification status:* ${CSCD428 ? "✅" : "❌"}`);
-        reply.push(`[🔰] *CSCD432 notification status:* ${CSCD432 ? "✅" : "❌"}`);
-        reply.push(`[🔰] *CSCD434 notification status:* ${CSCD434 ? "✅" : "❌"}`);
+            let reply = ['▄▀▄▀  𝔹𝕆𝕋 𝕊𝕋𝔸𝕋𝕌𝕊  ▀▄▀▄\n'];
+            reply.push(`[🔰] *Environment:* ${currentEnv}`);
+            reply.push(`[🔰] *Platform:* ${process.platform}`);
+            if (!RUN_FIRST_TIME) {
+                reply.push(`[🔰] *Response time:* ${Math.abs(new Date() - new Date(msg.timestamp * 1000))}ms`);
+            }
+            reply.push(`[🔰] *Uptime:*${days ? ' ' + days : ''}${days ? (days === 1 ? 'day' : 'days') : ''}${hours ? ' ' + hours : ''}${hours ? (hours === 1 ? 'hr' : 'hrs') : ''}${minutes ? ' ' + minutes : ' 0mins'}${minutes ? (minutes === 1 ? 'min' : 'mins') : ''} ${seconds ? seconds : 0}secs`);
+            reply.push(`[🔰] *Ram:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${Math.round(totalmem / 1024 / 1024)} MB`);
+            reply.push(`[🔰] *Total chats:* ${allChats.length}`);
+            reply.push(`[🔰] *Group chats:* ${groupChats}`);
+            reply.push(`[🔰] *Private chats:* ${private_chats}`);
+            reply.push(`[🔰] *Blocked chats:* ${blocked_chats.length}`);
+            reply.push(`[🔰] *Announcements stored:* ${allAnnouncements.length}`);
+            reply.push(`[🔰] *Links stored:* ${allLinks.length}`);
+            reply.push(`[🔰] *Forwarding stuff status:* ${isForwardingOn ? "✅" : "❌"}\n`); // temporary - will be removed soon
 
-        await msg.reply(reply.join('\n'));
+            reply.push(`[🔰] *CSCD416 notification status:* ${CSCD416 ? "✅" : "❌"}`);
+            reply.push(`[🔰] *CSCD418 notification status:* ${CSCD418 ? "✅" : "❌"}`);
+            reply.push(`[🔰] *CSCD422 notification status:* ${CSCD422 ? "✅" : "❌"}`);
+            reply.push(`[🔰] *CSCD424 notification status:* ${CSCD424 ? "✅" : "❌"}`);
+            reply.push(`[🔰] *CSCD400 notification status:* ${CSCD400 ? "✅" : "❌"}`);
+            reply.push(`[🔰] *CSCD426 notification status:* ${CSCD426 ? "✅" : "❌"}`);
+            reply.push(`[🔰] *CSCD428 notification status:* ${CSCD428 ? "✅" : "❌"}`);
+            reply.push(`[🔰] *CSCD432 notification status:* ${CSCD432 ? "✅" : "❌"}`);
+            reply.push(`[🔰] *CSCD434 notification status:* ${CSCD434 ? "✅" : "❌"}`);
+            return reply.join('\n');
+        }
+
+
+        // --------------------------------------------------------
+        let botLogGroup = null;
+        const logger = async () => {
+            const chats = await client.getChats();
+            botLogGroup = chats.find(chat => chat.pinned && chat.id.user === process.env.BOT_LOG_GROUP);
+
+            setInterval(async () => {
+                await botLogGroup.sendMessage(await generateReplies());
+            // }, 15_000);
+            }, 3600_000);
+        }
+
+        if (RUN_FIRST_TIME) {
+            logger();
+            args.RUN_FIRST_TIME = false;
+        } else {
+            await msg.reply(await generateReplies());
+        }
+
     } else {
         await msg.reply(pickRandomReply(NOT_BOT_ADMIN_REPLIES));
         return;
