fca40572a6778ee7e7f1b9d88d1f1221edbe07f4
CHR-onicles <tpandivine48@gmail.com>
2022-07-11 10:31:07 +0000

    fix: Minor fix

--- commands/academic/class.js
+++ commands/academic/class.js
@@ -1,7 +1,7 @@
 const { List } = require("whatsapp-web.js");
 const { getMutedStatus, getUsersToNotifyForClass } = require("../../models/misc");
 const { FOOTNOTES, REACT_EMOJIS } = require("../../utils/data");
-const { currentPrefix, todayClassReply, pickRandomWeightedMessage, pickRandomReply, currentEnv, PROD_PREFIX } = require("../../utils/helpers");
+const { currentPrefix, todayClassReply, pickRandomWeightedMessage, pickRandomReply, currentEnv } = require("../../utils/helpers");
 
 const execute = async (client, msg, args) => {
     if (await getMutedStatus() === true) return;
@@ -26,109 +26,109 @@ const execute = async (client, msg, args) => {
             temp && await chatFromContact.sendMessage(temp);
         }, 2000);
     }
-}
 
-// if user has already subscribed to be notified for class, get his elective and send the current day's
-// timetable based on the elective.
-if (multimedia.includes(contact.id.user)) {
-    helperForClassesToday(text, 'MA');
-    return;
-} else if (expert.includes(contact.id.user)) {
-    helperForClassesToday(text, 'E');
-    return;
-} else if (concurrent.includes(contact.id.user)) {
-    helperForClassesToday(text, 'C');
-    return;
-} else if (mobile.includes(contact.id.user)) {
-    helperForClassesToday(text, 'MC');
-    return;
-}
 
-const list = new List(
-    '\nMake a choice from the list of electives',
-    'See electives',
-    [{
-        title: 'Commands available to everyone',
-        rows: [
-            { id: lastPrefixUsed === process.env.DEV_PREFIX ? 'class-1_dev' : 'class-1_prod', title: 'Multimedia Applications', description: 'For those offering Multimedia Applications' },
-            { id: lastPrefixUsed === process.env.DEV_PREFIX ? 'class-2_dev' : 'class-2_prod', title: 'Expert Systems', description: "For those offering Expert Systems" },
-            { id: lastPrefixUsed === process.env.DEV_PREFIX ? 'class-3_dev' : 'class-3_prod', title: 'Conc & Distributed Systems', description: 'For those offering Concurrent & Distributed Systems' },
-            { id: lastPrefixUsed === process.env.DEV_PREFIX ? 'class-4_dev' : 'class-4_prod', title: 'Mobile Computing', description: 'For those offering Mobile Computing' },
-        ]
-    }],
-    'What elective do you offer?',
-    'Powered by Ethereal bot'
-);
-// console.log("[CLASS CMD] After creating list, lastPrefixedUsed:", lastPrefixUsed)
-
-!isListResponse && await chatFromContact.sendMessage(list);
-
-if (isListResponse) {
-    let text = "";
-    // console.log('[CLASS CMD] From class:', msg.selectedRowId);
-    // console.log('[CLASS CMD] Last prefix used in LR:', lastPrefixUsed)
-    const selectedRowId = msg.selectedRowId.split('-')[1];
-
-    // helper function for prevent redundancy
-    const helperFunc = async (elective) => {
-        text += await todayClassReply(text, elective);
-        // await msg.reply(text + `\nFrom ${currentEnv} env`);
-        await msg.reply(text);
-        setTimeout(async () => {
-            const temp = pickRandomWeightedMessage(FOOTNOTES);
-            temp && await chatFromContact.sendMessage(temp);
-        }, 2000);
+    // if user has already subscribed to be notified for class, get his elective and send the current day's
+    // timetable based on the elective.
+    if (multimedia.includes(contact.id.user)) {
+        helperForClassesToday(text, 'MA');
+        return;
+    } else if (expert.includes(contact.id.user)) {
+        helperForClassesToday(text, 'E');
+        return;
+    } else if (concurrent.includes(contact.id.user)) {
+        helperForClassesToday(text, 'C');
+        return;
+    } else if (mobile.includes(contact.id.user)) {
+        helperForClassesToday(text, 'MC');
+        return;
     }
 
-    switch (selectedRowId) {
-        case '1_dev':
-            if (currentEnv !== 'development') break;
-            helperFunc('MA');
-            break;
+    const list = new List(
+        '\nMake a choice from the list of electives',
+        'See electives',
+        [{
+            title: 'Commands available to everyone',
+            rows: [
+                { id: lastPrefixUsed === process.env.DEV_PREFIX ? 'class-1_dev' : 'class-1_prod', title: 'Multimedia Applications', description: 'For those offering Multimedia Applications' },
+                { id: lastPrefixUsed === process.env.DEV_PREFIX ? 'class-2_dev' : 'class-2_prod', title: 'Expert Systems', description: "For those offering Expert Systems" },
+                { id: lastPrefixUsed === process.env.DEV_PREFIX ? 'class-3_dev' : 'class-3_prod', title: 'Conc & Distributed Systems', description: 'For those offering Concurrent & Distributed Systems' },
+                { id: lastPrefixUsed === process.env.DEV_PREFIX ? 'class-4_dev' : 'class-4_prod', title: 'Mobile Computing', description: 'For those offering Mobile Computing' },
+            ]
+        }],
+        'What elective do you offer?',
+        'Powered by Ethereal bot'
+    );
+    // console.log("[CLASS CMD] After creating list, lastPrefixedUsed:", lastPrefixUsed)
 
-        case '1_prod':
-            if (currentEnv !== 'production') break;
-            helperFunc('MA');
-            break;
+    !isListResponse && await chatFromContact.sendMessage(list);
 
-        case '2_dev':
-            if (currentEnv !== 'development') break;
-            helperFunc('E');
-            break;
+    if (isListResponse) {
+        let text = "";
+        // console.log('[CLASS CMD] From class:', msg.selectedRowId);
+        // console.log('[CLASS CMD] Last prefix used in LR:', lastPrefixUsed)
+        const selectedRowId = msg.selectedRowId.split('-')[1];
 
-        case '2_prod':
-            if (currentEnv !== 'production') break;
-            helperFunc('E');
-            break;
+        // helper function for prevent redundancy
+        const helperFunc = async (elective) => {
+            text += await todayClassReply(text, elective);
+            // await msg.reply(text + `\nFrom ${currentEnv} env`);
+            await msg.reply(text);
+            setTimeout(async () => {
+                const temp = pickRandomWeightedMessage(FOOTNOTES);
+                temp && await chatFromContact.sendMessage(temp);
+            }, 2000);
+        }
 
-        case '3_dev':
-            if (currentEnv !== 'development') break;
-            helperFunc('C');
-            break;
+        switch (selectedRowId) {
+            case '1_dev':
+                if (currentEnv !== 'development') break;
+                helperFunc('MA');
+                break;
 
-        case '3_prod':
-            if (currentEnv !== 'production') break;
-            helperFunc('C');
-            break;
+            case '1_prod':
+                if (currentEnv !== 'production') break;
+                helperFunc('MA');
+                break;
 
-        case '4_dev':
-            if (currentEnv !== 'development') break;
-            helperFunc('MC');
-            break;
+            case '2_dev':
+                if (currentEnv !== 'development') break;
+                helperFunc('E');
+                break;
 
-        case '4_prod':
-            if (currentEnv !== 'production') break;
-            helperFunc('MC');
-            break;
+            case '2_prod':
+                if (currentEnv !== 'production') break;
+                helperFunc('E');
+                break;
 
-        default:
-            break;
+            case '3_dev':
+                if (currentEnv !== 'development') break;
+                helperFunc('C');
+                break;
+
+            case '3_prod':
+                if (currentEnv !== 'production') break;
+                helperFunc('C');
+                break;
+
+            case '4_dev':
+                if (currentEnv !== 'development') break;
+                helperFunc('MC');
+                break;
+
+            case '4_prod':
+                if (currentEnv !== 'production') break;
+                helperFunc('MC');
+                break;
+
+            default:
+                break;
+        }
+
+        args.isListResponse = false; // to prevent evaluating list response when message type is text
     }
 
-    args.isListResponse = false; // to prevent evaluating list response when message type is text
 }
-
-
 
 module.exports = {
     name: "class",
