2e9ee4ca833742ba938e0294372b5214ef6f12ec
CHR-onicles <tpandivine48@gmail.com>
2022-08-07 15:06:15 +0000

    removed dist folder

--- dist/src/interfaces/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../types");
-require("typescript");
-require("whatsapp-web.js");
--- dist/src/types/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../interfaces");
-require("whatsapp-web.js");
